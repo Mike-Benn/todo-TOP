4978dad769318bd876cfb7926a7fa3da3c737785
Mike-Benn <mbenn49@wgu.edu>
2024-02-17 03:27:00 -0500

    improve readability of code

--- src/pagecontroller.js
+++ src/pagecontroller.js
@@ -6,24 +6,46 @@ function PageController() {
     let general = Project("General");
     let today = Project("Today");
     let weekly = Project("Weekly");
+
     let activeProject = general;
+    let activeSidebar = general.getName();
+    let addProjectActive = "false";
     let staticProjects = ProjectList();
     staticProjects.addProject(general);
     staticProjects.addProject(today);
     staticProjects.addProject(weekly);
+    let dynamicProjects = ProjectList();
+    
 
     const getActiveProject = () => activeProject;
 
+    const getActiveSidebar = () => activeSidebar;
+
+    const isAddProjectActive = () => addProjectActive;
+
     const getStaticProjects = () => staticProjects;
 
-    const switchActiveProject = (project) => {
+    const getDynamicProjects = () => dynamicProjects;
+
+    const setActiveProject = (project) => {
         activeProject = project;
+        
+    }
+
+    const setAddProjectActive = (value) => {
+        addProjectActive = value;
+        
+        
     }
 
     return {
         getActiveProject,
+        getActiveSidebar,
+        isAddProjectActive,
         getStaticProjects,
-        switchActiveProject
+        getDynamicProjects,
+        setActiveProject,
+        setAddProjectActive
         
     };
 
